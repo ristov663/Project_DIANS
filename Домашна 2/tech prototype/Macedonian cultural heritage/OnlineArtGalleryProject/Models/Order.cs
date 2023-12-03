fa86e33a5fa0e93a1138010e379063266862353b
bozankailieva <bozankailieva1234@gmail.com>
2023-12-03 22:59:25 +0100

    Add files via upload

---
+++ Домашна 2/tech prototype/Macedonian cultural heritage/OnlineArtGalleryProject/Models/Order.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OnlineArtGalleryProject.Models
+{
+    public class Order
+    {
+        public int OrderId { get; set; }
+        public System.DateTime OrderDate { get; set; }
+        public string OrderNumber { get; set; }
+    }
+}
