695b2e8eb5af8e702f03c1f63dce2da92658109b
eftimovvs <109041854+eftimovvs@users.noreply.github.com>
2023-12-24 20:24:50 +0100

    upload Domasna3

---
+++ Macedonian cultural heritage/OnlineArtGalleryProject/Models/Category.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OnlineArtGalleryProject.Models
+{
+   
+        public enum Category
+        {
+
+            Churches, 
+            Paintings, 
+            Museums, 
+            Archaeological_sites, 
+            Statues, 
+            Castles,
+            Mosques,
+            Bridges, 
+            Theaters, 
+            Objects, 
+            Fortresses, 
+            Buildings,
+            Mosaics,
+            Constructions,
+            Sculpture
+    }
+    
+}
